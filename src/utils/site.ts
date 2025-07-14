c8996edb2c694c9abf3a4c57f6f7733bc55596bd
sesukneksa <sesukneksa@gmail.com>
2025-07-15 01:13:45 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Indo Bocah';
-export const desk = 'Video Bokep Indo Jepang Jav Hijab Tante Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
-export const url = 'https://indobocah.pages.dev';
-export const terbit = '2025-06-27T00:01:03Z';
